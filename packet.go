e3a475a25513a93223f53b6dff33a8e72334be50
Nick Rosbrook <rosbrookn@ainfosec.com>
2019-03-19 21:03:56 -0400

    Implement packet formatting
    Signed-off-by: Nick Rosbrook <rosbrookn@ainfosec.com>

--- packet.go
+++ packet.go
@@ -1,31 +1,80 @@
 // Package vici implements a strongSwan vici protocol client
 package vici
 
-// PacketType represents the packet type
-type PacketType uint8
+import (
+	"bytes"
+)
 
 const (
 	// A name request message
-	CmdRequest = iota
+	pktCmdRequest uint8 = iota
 
 	// An unnamed response message for a request
-	CmdResponse
+	pktCmdResponse
 
 	// An unnamed response if requested command is unknown
-	CmdUnkown
+	pktCmdUnkown
 
 	// A named event registration request
-	EventRegister
+	pktEventRegister
 
 	// A name event deregistration request
-	EventUnregister
+	pktEventUnregister
 
 	// An unnamed response for successful event (de-)registration
-	EventConfirm
+	pktEventConfirm
 
 	// An unnamed response if event (de-)registration failed
-	EventUnknown
+	pktEventUnknown
 
 	// A named event message
-	Event
+	pktEvent
 )
+
+// A packet has a required type (an 8-bit identifier), a name (only required for named types),
+// and and an optional message field.
+type packet struct {
+	ptype uint8
+	name  string
+
+	msg *message
+}
+
+func newPacket(ptype uint8, name string, msg *message) *packet {
+	return &packet{
+		ptype: ptype,
+		name:  name,
+		msg:   msg,
+	}
+}
+
+// bytes formats the packet and returns it as a byte slice
+func (p *packet) bytes() ([]byte, error) {
+	// Create a new buffer with the first byte indicating the packet type
+	buf := bytes.NewBuffer([]byte{p.ptype})
+
+	// Write the name, preceeded by its length
+	err := buf.WriteByte(uint8(len(p.name)))
+	if err != nil {
+		return []byte{}, err
+	}
+
+	_, err := buf.WriteString(p.name)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	if msg != nil {
+		b, err := msg.encode()
+		if err != nil {
+			return []byte{}, err
+		}
+
+		_, err := buf.Write(b)
+		if err != nil {
+			return []byte{}, err
+		}
+	}
+
+	return buf.Bytes(), nil
+}
