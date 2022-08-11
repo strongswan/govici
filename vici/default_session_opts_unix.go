426d5e43cf5abfff950fecbcce5c8f85c3b4e45d
Nick Rosbrook <nr@enr0n.net>
2022-08-11 15:27:33 -0400

    session: set GOOS appropriate defaults at build time
    The vici plugin uses a Unix socket by default on Linux, and uses TCP by
    default on Windows. Set these defaults appropriately at build time.
    
    Closes #44
    
    Signed-off-by: Nick Rosbrook <nr@enr0n.net>

--- vici/default_session_opts_unix.go
+++ vici/default_session_opts_unix.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 Nick Rosbrook
+// Copyright (C) 2022 Nick Rosbrook
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -18,74 +18,11 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+//go:build !windows
+
 package vici
 
-import (
-	"bytes"
-	"encoding/binary"
-	"io"
-	"net"
-)
-
 const (
-	// Each segment is prefixed by a 4-byte header in network oreder
-	headerLength = 4
+	sessionDefaultNetwork = "unix"
+	sessionDefaultAddr    = "/var/run/charon.vici"
 )
-
-type transport struct {
-	conn net.Conn
-}
-
-func (t *transport) send(pkt *packet) error {
-	buf := bytes.NewBuffer([]byte{})
-
-	b, err := pkt.bytes()
-	if err != nil {
-		return err
-	}
-
-	// Write the packet length
-	pl := make([]byte, headerLength)
-	binary.BigEndian.PutUint32(pl, uint32(len(b)))
-	_, err = buf.Write(pl)
-	if err != nil {
-		return err
-	}
-
-	// Write the payload
-	_, err = buf.Write(b)
-	if err != nil {
-		return err
-	}
-
-	_, err = t.conn.Write(buf.Bytes())
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (t *transport) recv() (*packet, error) {
-	buf := make([]byte, headerLength)
-
-	_, err := io.ReadFull(t.conn, buf)
-	if err != nil {
-		return nil, err
-	}
-	pl := binary.BigEndian.Uint32(buf)
-
-	buf = make([]byte, int(pl))
-	_, err = io.ReadFull(t.conn, buf)
-	if err != nil {
-		return nil, err
-	}
-
-	p := &packet{}
-	err = p.parse(buf)
-	if err != nil {
-		return nil, err
-	}
-
-	return p, nil
-}
