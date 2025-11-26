054c3f1175ac2f850b12291db85b4180d721c24d
Nick Rosbrook <nr@enr0n.net>
2025-11-26 17:39:12 -0500

    message: stop decoding if a key is empty
    Detected by FuzzEncodeDecode/318acd41d45f4ac3.

--- vici/message.go
+++ vici/message.go
@@ -770,6 +770,9 @@ func (m *Message) decodeKeyValue(data []byte) (int, error) {
 	if err != nil {
 		return -1, fmt.Errorf("%v: %v", errDecoding, err)
 	}
+	if n == 0 {
+		return -1, fmt.Errorf("%v: key cannot be empty", errDecoding)
+	}
 
 	keyLen := int(n)
 	key := string(buf.Next(keyLen))
@@ -812,6 +815,9 @@ func (m *Message) decodeList(data []byte) (int, error) {
 	n, err := buf.ReadByte()
 	if err != nil {
 		return -1, fmt.Errorf("%v: %v", errDecoding, err)
+	}
+	if n == 0 {
+		return -1, fmt.Errorf("%v: key cannot be empty", errDecoding)
 	}
 
 	keyLen := int(n)
@@ -884,6 +890,9 @@ func (m *Message) decodeSection(data []byte) (int, error) {
 	n, err := buf.ReadByte()
 	if err != nil {
 		return -1, fmt.Errorf("%v: %v", errDecoding, err)
+	}
+	if n == 0 {
+		return -1, fmt.Errorf("%v: key cannot be empty", errDecoding)
 	}
 
 	keyLen := int(n)
